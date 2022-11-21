d8e2aaba026f60b2a7b1fdd9dbf2ad9b6b47d49f
AzerDime <guyian@pdx.edu>
2022-11-21 15:21:20 -0800

    added gitignore, prototype routing. using EJS

--- routing.js
+++ routing.js
@@ -1,1 +1,25 @@
 //will use js for routing between HTML files when GET from onclicks
+const express = require("express");
+const ejs = require("ejs")
+const app = express();
+const router = express.Router();
+
+app.set("view engine", "ejs");
+
+router.get("home", (req, res) => {
+    res.render("index");
+});
+
+router.get("contact", (req, res) => {
+    res.render("contact");
+});
+
+router.get("prevWorkProj", (req, res) => {
+    res.render("prevWorkProj");
+});
+
+app.use(router);
+
+app.get("home", router);
+app.get("contact", router);
+app.get("prevWorkProj", router);
